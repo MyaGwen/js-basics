72cafbe3791a24540d08477cc9c3efcb7be0f1e0
MyaGwen <wendy.e.ahadome@gmail.com>
2023-02-12 05:30:14 -0800

    commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//Write a function that multiples two numbers. Call that function to multiply any two numbers of your choice and log the answer in your console
+
+let a = 11;
+let b = 5;
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(a, b));
+
+
+// create variables for your first name, last name, Stutern track, skills, favourite color, year of your country’s independence. Use these variables to form a sentence and log the sentence in the console.
+
+const fName = "Wendy";
+const lName = "Ahadome";
+let sTrack = "Software Engineering";
+let skills = "HTML, CSS, JavaScript";
+let fColor = "peach";
+let countryInd = 1957;
+
+console.log("My name is " + fName + " " + lName + ". " + "My favourite color is " + fColor + " and my beloved country Ghana gained her independence in the year " + countryInd + ". " + "I'm currently enrolled in cohort 1.4 of " + "Stutern's " + sTrack + " track.");
